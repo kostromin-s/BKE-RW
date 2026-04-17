4e8ddf70f83cd460cc0c967cfbca51f814aa472e
kostromin-s <nam.lh220118@sis.hust.edu.vn>
2026-04-17 14:37:55 +0700

    detect type

--- biological_random_walks/personalization_vector_creation/pv_creation.py
+++ biological_random_walks/personalization_vector_creation/pv_creation.py
@@ -1,7 +1,9 @@
 import abc
 
+from typing import Dict, Any
+import abc
+
 class PersonalizationVectorCreation(metaclass=abc.ABCMeta):
-	
-	@abc.abstractmethod
-	def run(self):
-		print("abstract methods")
+    @abc.abstractmethod
+    def run(self) -> Dict[Any, float]:
+        pass
